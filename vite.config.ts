2e8e8438336d714b63fcd8b18ce1122b09dd7cc7
Mekill404 <hei.mahery.55@gmail.com>
2026-06-26 06:00:00 +0200

    chore: initialiser la configuration du projet

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,40 @@
+import path from "path"
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
     server: {
         host: true,
+        port: 5173,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+                configure: (proxy, options) => {
+                    proxy.on('error', (err, req, res) => {
+                        console.log('proxy error', err);
+                    });
+                    proxy.on('proxyReq', (proxyReq, req, res) => {
+                        console.log('Sending Request to the Target:', req.method, req.url);
+                    });
+                    proxy.on('proxyRes', (proxyRes, req, res) => {
+                        console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+                    });
+                },
+            },
+        },
     },
     base: './',
     build: {
-        sourcemap: mode === "developement",
+        sourcemap: mode === "development",
     },
     test: {
         globals: true,
